7304bf4c4d0e2d538352e52a3526b42a25cc7581
Marcos Reis <marcosreis09122000@gmail.com>
2021-02-10 11:29:36 -0300

    intro

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var nome = "Marcos Reis";
+var idade = 20;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert(`Bem vindo ${nome}`);
+//alert(idade + idade2);
+//Caso os valores fossem strings os valores seriam concatenados, não somados
+console.log(idade + idade2) 
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.replace("Japão", "Itália"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
